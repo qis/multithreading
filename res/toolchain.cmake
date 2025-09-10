0a2f7948ca85f2c90c0aa4b49a599888e77d0740
Alexej Harm <a.harm@avsw.ru>
2025-09-10 13:09:04 +0300

    use vcpkg instead of vcvars

--- res/toolchain.cmake
+++ res/toolchain.cmake
@@ -3,9 +3,6 @@ include_guard(GLOBAL)
 if(CMAKE_BUILD_TYPE)
   mark_as_advanced(CMAKE_BUILD_TYPE)
 endif()
-if(CMAKE_CONFIGURATION_TYPES)
-  mark_as_advanced(CMAKE_CONFIGURATION_TYPES)
-endif()
 if(CMAKE_VERBOSE_MAKEFILE)
   mark_as_advanced(CMAKE_VERBOSE_MAKEFILE)
 endif()
@@ -15,16 +12,17 @@ endif()
 
 # Vcpkg
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-  set(VSWHERE_ROOT "$ENV{ProgramFiles\(x86\)}/Microsoft Visual Studio/Installer")
-  cmake_path(CONVERT "${VSWHERE_ROOT}" TO_CMAKE_PATH_LIST VSWHERE_ROOT NORMALIZE)
-
-  find_program(VSWHERE NAMES vswhere REQUIRED PATHS "${VSWHERE_ROOT}")
-  list(APPEND VSWHERE -nologo -nocolor -utf8 -format value -property resolvedInstallationPath)
-  list(APPEND VSWHERE -products "*" -requires Microsoft.VisualStudio.Component.Vcpkg -latest)
-  execute_process(COMMAND ${VSWHERE} ENCODING UTF-8 OUTPUT_VARIABLE VS_ROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
-  cmake_path(CONVERT "${VS_ROOT}" TO_CMAKE_PATH_LIST VS_ROOT NORMALIZE)
-
-  find_program(VCPKG NAMES vcpkg PATHS "${VS_ROOT}/VC/vcpkg")
+  find_program(VCPKG NAMES vcpkg)
+  if(NOT VCPKG)
+    set(VSWHERE_ROOT "$ENV{ProgramFiles\(x86\)}/Microsoft Visual Studio/Installer")
+    cmake_path(CONVERT "${VSWHERE_ROOT}" TO_CMAKE_PATH_LIST VSWHERE_ROOT NORMALIZE)
+    find_program(VSWHERE NAMES vswhere REQUIRED PATHS "${VSWHERE_ROOT}")
+    list(APPEND VSWHERE -nologo -nocolor -utf8 -format value -property resolvedInstallationPath)
+    list(APPEND VSWHERE -products "*" -requires Microsoft.VisualStudio.Component.Vcpkg -latest)
+    execute_process(COMMAND ${VSWHERE} ENCODING UTF-8 OUTPUT_VARIABLE VS_ROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
+    cmake_path(CONVERT "${VS_ROOT}" TO_CMAKE_PATH_LIST VS_ROOT NORMALIZE)
+    find_program(VCPKG NAMES vcpkg PATHS "${VS_ROOT}/VC/vcpkg")
+  endif()
   set(VCPKG_TARGET_TRIPLET "x64-windows-static")
 elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
   find_program(VCPKG NAMES vcpkg PATHS /opt/vcpkg)
@@ -39,6 +37,7 @@ if(VCPKG)
   set(VCPKG_INSTALL_OPTIONS "--x-no-default-features")
   set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--clean-buildtrees-after-build")
   set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--clean-packages-after-build")
+  #set(VCPKG_INSTALL_OPTIONS "${VCPKG_INSTALL_OPTIONS};--no-print-usage")
 
   if(NOT DEFINED ENV{VCPKG_DOWNLOADS})
     set(ENV{VCPKG_DOWNLOADS} "${CMAKE_SOURCE_DIR}/.cache/vcpkg/downloads")
@@ -48,11 +47,12 @@ if(VCPKG)
     set(ENV{VCPKG_BINARY_SOURCES} "clear;files,${CMAKE_SOURCE_DIR}/.cache/vcpkg/binaries,readwrite")
   endif()
 
-  set(ENV{VCPKG_FORCE_SYSTEM_BINARIES} "ON")
+  #set(ENV{VCPKG_FORCE_SYSTEM_BINARIES} "ON")
   set(ENV{VCPKG_DISABLE_METRICS} "ON")
   set(VCPKG_APPLOCAL_DEPS OFF)
 
   include("${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+  include("${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/toolchain.cmake")
 
   file(GLOB TOOLCHAIN_DLLS "${CMAKE_BINARY_DIR}/vcpkg_installed/${VCPKG_TARGET_TRIPLET}/bin/*.dll")
   foreach(TOOLCHAIN_DLL ${TOOLCHAIN_DLLS})
@@ -63,9 +63,27 @@ if(VCPKG)
   set(VCPKG_FOUND ON CACHE BOOL "")
 endif()
 
-# Debug
-set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+# Standard
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+  add_compile_options(/std:c++latest /utf-8)
+else()
+  set(CMAKE_CXX_STANDARD 26 CACHE STRING "")
+  set(CMAKE_CXX_EXTENSIONS OFF CACHE BOOL "")
+  set(CMAKE_CXX_SCAN_FOR_MODULES OFF CACHE BOOL "")
+endif()
+
+# Compiler Flags
+cmake_policy(SET CMP0092 NEW)
+string(APPEND CMAKE_C_FLAGS_INIT " /W4 /wd4100")
+string(APPEND CMAKE_CXX_FLAGS_INIT " /W4 /wd4100")
+
+# Debug Information
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded")
 add_compile_definitions($<$<CONFIG:Debug>:_GLIBCXX_DEBUG>)
+
+# Configurations
+set_property(GLOBAL PROPERTY DEBUG_CONFIGURATIONS Debug)
+set(CMAKE_MAP_IMPORTED_CONFIG_DEBUG ";Release" CACHE STRING "")
 
 # Output
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
